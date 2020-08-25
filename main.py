84e2a2b74de19daa3d0cce0332a1557ca3bb008d
luba.kapustina <luba.kapustina@gmail.com>
2020-08-25 17:47:16 +0300

    update readme

---
+++ main.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+import math
+import my_library
+import my_math.my_constant as ct
+# import my_package.my_function as fun
+from my_package.my_function import divide_two_numbers as divis
+
+# fun.say_hello()
+
+# w = fun.divide_two_numbers(a=2, b=3)
+# print(w)
+
+# w = divide_two_numbers(a=2, b=3)
+# print(w)
+
+w = divis(a=2, b=3)
+print(w)
+
+
+# print(ct.e)
+# print(ct.k)
+
+# a = 10
+# # print(f"a value = {a}, a type = {type(a)}")
+#
+# b = 1.5
+# # print(f"b value = {b}, a type = {type(b)}")
+# #
+# # c = a * b
+# #
+# # print(f"c value = {c}, a type = {type(c)}")
+# #
+# #
+# # d = a % b #остаток от целочисленного деления а на b
+# #
+# # print(f"d value = {d}, a type = {type(d)}")
+# #
+# # e = a ** b
+# # print(f"e value = {e}, a type = {type(e)}")
+#
+# c = a ** 3
+# d = math.pi
+# print(d)
+#
+# # f = a ** 1.5
+# # print(f)
+#
+# k = my_library.pi
+# print(k)
+
+
+
+
+
+
+
+
+
+
+
---
+++ my_package/my_function.py
@@ -0,0 +1,32 @@
+
+def divide_two_numbers(a, b):
+    c = a / b
+    return c
+
+
+def say_hello():
+    print("hello!")
+    print("how")
+    print("are")
+    print("you?")
+
+
+if __name__ == '__main__':
+    # a = 2
+    # b = 3
+    # result = sum_two_numbers(a, b)
+    # print(result)
+    #
+
+    # result = sum_two_numbers(a=2, b=3)
+    # print(result)
+
+    # result = sum_two_numbers(3, 2)
+    # print(result)
+
+    res1 = 2
+    res2 = 3
+
+    result = divide_two_numbers(b=res1, a=res2)
+    print(result)
+
