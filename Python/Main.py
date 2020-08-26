650b25d4f6314dce7926e84a6eb0957a00999547
nikitachunikhin <nikita.chunikhin@gmail.com>
2020-08-26 18:11:31 +0300

    creat new function

--- Python/Main.py
+++
@@ -1,4 +0,0 @@
-from Python.gg import mnog as mnog
-from Python.gg import min as min
-w = mnog(a=5, b=3)
-print (w)
--- tolya/gg.py
+++ tolya/gg.py
@@ -1,18 +1,18 @@
 import math
 
-def divi (a, b):
+def divi(a, b):
     c = a/b
     return c
 
-def mnog (a, b):
+def mnog(a, b):
     c = a*b
     return c
 
-def dod (a, b):
+def dod(a, b):
     c = a+b
     return c
 
-def min (a, b):
+def min(a, b):
     c = a-b
     return c
 
