cb8ef4d60871bfeb3f32e8b6acac8bb54e892b35
luba.kapustina <luba.kapustina@gmail.com>
2020-08-26 18:39:24 +0300

    update readme

--- my_package/my_function.py
+++ my_package/my_function.py
@@ -6,23 +6,21 @@ def divide_two_numbers(a, b):
 
 def say_hello():
     print("hello!")
-    print("how")
-    print("are")
-    print("you?")
+
 
 
 if __name__ == '__main__':
-    # a = 2
-    # b = 3
-    # result = sum_two_numbers(a, b)
-    # print(result)
-    #
+    a = 2
+    b = 3
+    result = sum_two_numbers(a, b)
+    print(result)
 
-    # result = sum_two_numbers(a=2, b=3)
-    # print(result)
 
-    # result = sum_two_numbers(3, 2)
-    # print(result)
+    result = sum_two_numbers(a=2, b=3)
+    print(result)
+
+    result = sum_two_numbers(3, 2)
+    print(result)
 
     res1 = 2
     res2 = 3
