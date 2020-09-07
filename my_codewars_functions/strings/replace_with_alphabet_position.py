b83f6ad4d494832c007e84d7ffcda46d6ff5d92f
luba.kapustina <luba.kapustina@gmail.com>
2020-09-07 12:42:10 +0300

    update and order project

--- my_codewars_functions/strings/replace_with_alphabet_position.py
+++ my_codewars_functions/strings/replace_with_alphabet_position.py
@@ -12,6 +12,23 @@ def alphabet_position(text):
     output_result = output_result.strip()
     return output_result
 
+def alphabet_position2(text):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if type(text) == str:
+        text = text.lower()
+        result = ''
+        if text.isalpha():
+            for letter in text:
+                result = result + ' ' + str(alphabet.index(letter) + 1)
+            print('only text')
+            return result.lstrip(' ')
+        else:
+            for letter in text:
+                if letter.isalpha():
+                    result = result + ' ' + str(alphabet.index(letter) + 1)
+            print('other symbols')
+            return result.lstrip(' ')
+
 if __name__ == '__main__':
     pass
     res = alphabet_position("The sunset sets at twelve o' clock.")
